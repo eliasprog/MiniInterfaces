df95a381299155c5c1c50ea8f4d013b817ff6ae5
Lucas Alves <tilucasoli@gmail.com>
2020-04-29 11:06:43 -0300

    archive: appColors

---
+++ MiniInterfaces/Controller/AddMomentoController.swift
@@ -0,0 +1,23 @@
+//
+//  AddMomentoController.swift
+//  MiniInterfaces
+//
+//  Created by Lucas Oliveira on 29/04/20.
+//  Copyright © 2020 Elias Ferreira. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddMomentoController: UIViewController {
+
+    @IBOutlet weak var largeTitle: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        largeTitle.font = UIFont(name:"SFRounded-Bold", size: 34.0)
+    }
+
+
+}
+
