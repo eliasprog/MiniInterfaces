484200c6b957c288066c988ab64f2936c9c33616
Elias Ferreira <eliasferreira.pro@gmail.com>
2020-04-29 11:26:06 -0300

    Alteracoes na tela Momentos

---
+++ MiniInterfaces/Controller/MomentosController.swift
@@ -0,0 +1,18 @@
+//
+//  MomentosController.swift
+//  MiniInterfaces
+//
+//  Created by Elias Ferreira on 29/04/20.
+//  Copyright © 2020 Elias Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class MomentosController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
