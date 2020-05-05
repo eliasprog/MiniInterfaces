19b543db2c44c58c9c0ecf80934d62240f1a9090
Lucas Alves <tilucasoli@gmail.com>
2020-05-04 23:03:53 -0300

    Tela AddMomento Finalizada

---
+++ MiniInterfaces/Controller/AddMomento2ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddMomentos2ViewController.swift
+//  MiniInterfaces
+//
+//  Created by Lucas Oliveira on 04/05/20.
+//  Copyright © 2020 Elias Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class AddMomentos2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
