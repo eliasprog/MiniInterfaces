29fc5a8f8a6fe2173ddc97856544758a3c6ca0b3
Elias Ferreira <eliasferreira.pro@gmail.com>
2020-05-06 12:29:19 -0300

    -Tela Add Momento 3 criada. - Card com tamanho dinamico.

--- MiniInterfaces/View/AddMomento3Controller.swift
+++ MiniInterfaces/View/AddMomento3Controller.swift
@@ -10,21 +10,47 @@ import UIKit
 
 class AddMomento3Controller: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var card: Card?
+    
+    @IBAction func saveAndClose(_ sender: Any) {
+        self.navigationController?.popToRootViewController(animated: true)
+        print("close")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let feelingRadiante = FeelingCard(color: .radianteColor, name: "Radiante")
+        card = Card(
+        photo: "cabelo",
+        feelingColor: feelingRadiante,
+        title: "Pintei o cabelo",
+        description: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf"
+        )
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.register(UINib.init(nibName: "CardCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "CardCell")
     }
-    */
+    
+}
 
+extension AddMomento3Controller: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath) as! CardCollectionViewCell
+        
+        cell.configure(with: self.card!)
+        
+        return cell
+    }
+    
+    
 }
